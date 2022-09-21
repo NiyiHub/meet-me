a1cc235cec2dbcc4c912d5c6c465c1787c28a4b6
NiyiHub <niyiblack@outlook.com>
2022-09-21 06:14:50 +0100

    Index page modified.

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'landing_page/static/')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'landing_page/assets/')
+# ]
 
 STATIC_ROOT = BASE_DIR / 'assets'
 
